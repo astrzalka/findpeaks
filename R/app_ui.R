9bd06b00b8dde813967cbcfe4d4dda85022e2986
agnieszka <the.larch19@gmail.com>
2020-08-06 13:45:51 +0200

    added more colorbrewer scales

--- R/app_ui.R
+++ R/app_ui.R
@@ -152,7 +152,9 @@ app_ui <- function(request) {
                                            conditionalPanel(
                                              condition = "input.kolory_hist == 'colorbrewer'",
                                              selectInput('colorbrewer_hist', label = 'Którą skalę Colorbrewer zastosować?',
-                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent'),
+                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent',
+                                                                     'Spectral' ,'RdYlGn','RdYlBu','RdBu', 'PuOr','PRGn','PiYG', 'YlOrRd','YlGnBu',
+                                                                     'PuBuGn','Blues', 'YlGn', 'Reds', 'RdPu', 'Purples', 'OrRd', 'GnBu' ),
                                                          selected = 'Set1', multiple = FALSE)
                                            ),
                                            conditionalPanel(
@@ -180,7 +182,9 @@ app_ui <- function(request) {
                                            conditionalPanel(
                                              condition = "input.kolory_dens == 'colorbrewer'",
                                              selectInput('colorbrewer_dens', label = 'Którą skalę Colorbrewer zastosować?',
-                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent'),
+                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent',
+                                                                     'Spectral' ,'RdYlGn','RdYlBu','RdBu', 'PuOr','PRGn','PiYG', 'YlOrRd','YlGnBu',
+                                                                     'PuBuGn','Blues', 'YlGn', 'Reds', 'RdPu', 'Purples', 'OrRd', 'GnBu' ),
                                                          selected = 'Set1', multiple = FALSE)
                                            ),
                                            conditionalPanel(
@@ -238,7 +242,9 @@ app_ui <- function(request) {
                                            conditionalPanel(
                                              condition = "input.kolory == 'colorbrewer'",
                                              selectInput('colorbrewer', label = 'Którą skalę Colorbrewer zastosować?',
-                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent'),
+                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent',
+                                                                     'Spectral' ,'RdYlGn','RdYlBu','RdBu', 'PuOr','PRGn','PiYG', 'YlOrRd','YlGnBu',
+                                                                     'PuBuGn','Blues', 'YlGn', 'Reds', 'RdPu', 'Purples', 'OrRd', 'GnBu' ),
                                                          selected = 'Set1', multiple = FALSE)
                                            ),
                                            conditionalPanel(
@@ -302,8 +308,10 @@ app_ui <- function(request) {
                                            conditionalPanel(
                                              condition = "input.kolory_scatter == 'colorbrewer'",
                                              selectInput('colorbrewer_scatter', label = 'Którą skalę Colorbrewer zastosować?',
-                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent'),
-                                                         selected = 'Set1', multiple = FALSE)
+                                                         choices = c('Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Dark2', 'Accent',
+                                                                     'Spectral' ,'RdYlGn','RdYlBu','RdBu', 'PuOr','PRGn','PiYG', 'YlOrRd','YlGnBu',
+                                                                     'PuBuGn','Blues', 'YlGn', 'Reds', 'RdPu', 'Purples', 'OrRd', 'GnBu' ),
+                                                         selected = 'Spectral', multiple = FALSE)
                                            ),
                                            conditionalPanel(
                                              condition = "input.kolory_scatter == 'viridis'",
